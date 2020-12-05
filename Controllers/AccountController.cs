3395a703b091463028b71d28e387e086833f5437
Hau Nguyen <huutho1999@gmail.com>
2020-12-05 18:18:28 +0700

    Cap nhat giao dien Them san pham tu trang quan ly

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -4,12 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Ecommerce.Data;
 
 namespace Ecommerce.Controllers
 {
     public class AccountController : Controller
     {
         // TODO: hoàn thành trang login, logout và register
+        private readonly ApplicationDbContext dbContext;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AccountController(ApplicationDbContext _context, RoleManager<IdentityRole> _roleManager)
+        {
+            dbContext = _context;
+            roleManager = _roleManager;
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult LoginAdmin()
+        {
+            return View();
+        }
     }
 }

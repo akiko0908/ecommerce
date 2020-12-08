f0caef8a83c0ef8c66a3e68094059f0be2826802
Hau Nguyen <huutho1999@gmail.com>
2020-12-08 22:07:19 +0700

    Hoan thanh cac chuc nang quan ly duoi quyen Administrator

--- Areas/Admin/Controllers/ManageBrandController.cs
+++ Areas/Admin/Controllers/ManageBrandController.cs
@@ -7,6 +7,9 @@ using Ecommerce.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce.Areas.Admin.Controllers
 {
@@ -23,9 +26,9 @@ namespace Ecommerce.Areas.Admin.Controllers
         public IActionResult Index()
         {
             var lsBrand = dbContext.Brands;
-            if (TempData["SuccessDelete"] != null)
+            if (TempData["notifyMsg"] != null)
             {
-                ViewBag.DeleteMsg = TempData["SuccessDelete"];
+                ViewBag.notifyMsg = TempData["notifyMsg"];
             }
             return View(lsBrand.ToList());
         }
@@ -33,87 +36,77 @@ namespace Ecommerce.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult Create()
         {
-            return PartialView();
+            if (TempData["notifyMsg"] != null)
+            {
+                ViewBag.notifyMsg = TempData["notifyMsg"];
+            }
+            return View();
         }
+
+        [HttpPost]
         public IActionResult Create(Brand newBrand)
         {
             if (ModelState.IsValid)
             {
                 dbContext.Brands.Add(newBrand);
                 dbContext.SaveChanges();
+                TempData["notifyMsg"] = "Thương Hiệu mới được thêm thành công!!!";
+                return RedirectToAction("Index");
             }
-            return RedirectToAction("Index");
+            TempData["notifyMsg"] = "Thêm Thương hiệu mới thất bại do đã tồn tại hoặc không đúng!!!";
+            return View(newBrand);
         }
 
         [HttpGet]
         public IActionResult Edit(int? id)
         {
             Brand brand = dbContext.Brands.Find(id);
-            return PartialView(brand);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(int? id, string nameBrandChange)
-        {
-            Brand oldBrand = dbContext.Brands.Find(id);
-            if (oldBrand.brand_Name.CompareTo(nameBrandChange) == 0)
+            if (TempData["notifyMsg"] != null)
             {
-                oldBrand.brand_Name = nameBrandChange;
-                dbContext.SaveChanges();
-                return RedirectToAction("Index");
+                ViewBag.notifyMsg = TempData["notifyMsg"];
             }
-            return RedirectToAction("Edit", "Brand", oldBrand);
+            return View(brand);
+        }
+        [HttpPost]
+        public IActionResult Edit(int? id, Brand updateBrand)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id != null)
+                {
+                    Brand oldBrand = dbContext.Brands.Find(id);
+                    if (oldBrand.brand_ID == updateBrand.brand_ID && oldBrand.brand_Name != updateBrand.brand_Name)
+                    {
+                        oldBrand.brand_Name = updateBrand.brand_Name;
+                        dbContext.SaveChanges();
+                        TempData["notifyMsg"] = "Cập nhật thông tin Thương hiệu thành công!!!";
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            TempData["notifyMsg"] = "Cập nhật thông tin Thương hiệu thất bại";
+            return View(updateBrand);
         }
 
         [HttpGet]
         public IActionResult Delete(int? id)
         {
             Brand oldBrand = dbContext.Brands.Find(id);
-            return PartialView(oldBrand);
+            return View(oldBrand);
         }
-
         [HttpPost]
-        public IActionResult Delete(Brand oldBrand)
+        public IActionResult ConfirmDelete(int? id)
         {
-            if (ModelState.IsValid)
+            if (id != null)
             {
-                if (oldBrand != null)
-                {
-                    dbContext.Remove(oldBrand);
-                    dbContext.SaveChanges();
-                    TempData["SuccessDelete"] = "Xóa Hãng sản xuất thành công!!!";
-                    return RedirectToAction("Index");
-                }
+                Brand deleteBrand = dbContext.Brands.Find(id);
+                dbContext.Remove(deleteBrand);
+                dbContext.SaveChanges();
+                TempData["notifyMsg"] = "Xóa Hãng sản xuất thành công!!!";
+                return RedirectToAction("Index");
             }
+            TempData["notifyMsg"] = "Xóa Thương hiệu thất bại!!!";
             return RedirectToAction("Index");
-        }
-
-        public IActionResult Details(int? id)
-        {
-            Brand chooseBrand = dbContext.Brands.Find(id);
-            return PartialView(chooseBrand);
-        }
-
-        [HttpGet]
-        public IActionResult Update(int? id)
-        {
-            Brand chooseBrand = dbContext.Brands.Find(id);
-            return PartialView(chooseBrand);
-        }
-
-        [HttpPost]
-        public IActionResult Update(Brand chooseBrand)
-        {
-            if (ModelState.IsValid)
-            {
-                if (chooseBrand != null)
-                {
-                    dbContext.Brands.Remove(chooseBrand);
-                    dbContext.SaveChanges();
-                    return RedirectToAction("Index");
-                }
-            }
-            return PartialView("Update", chooseBrand);
         }
     }
 }
--- Areas/Admin/Controllers/ManageProductController.cs
+++ Areas/Admin/Controllers/ManageProductController.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Ecommerce.Data;
 using Ecommerce.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce.Areas.Admin.Controllers
 {
@@ -21,52 +24,140 @@ namespace Ecommerce.Areas.Admin.Controllers
             dbContext = _context;
         }
 
-        [Area("Admin")]
         public IActionResult Index()
         {
-            var lsProduct = dbContext.Products
-                                        .Include(x => x.Brand)
-                                        .Include(x => x.HeDieuHanh)
-                                        .Include(x => x.Supplier)
-                                        .Include(x => x.Categories);
+            var lsProduct = dbContext.Products.Include(x => x.Brand)
+                                              .Include(x => x.HeDieuHanh)
+                                              .Include(x => x.Supplier)
+                                              .Include(x => x.Categories);
+
             if (TempData["notifyMSG"] != null)
             {
                 ViewBag.notifyMsg = TempData["notifyMSG"];
             }
+
             return View(lsProduct.ToList());
         }
 
         public IActionResult Detail(int? id)
         {
-            var product = dbContext.Products
-                                        .Include(x => x.Brand)
-                                        .Include(x => x.HeDieuHanh)
-                                        .Include(x => x.Supplier)
-                                        .Include(x => x.Categories)
-                                        .Where(x => x.product_ID == id)
-                                        .FirstOrDefault();
-            return PartialView("Detail", product);
+            var product = dbContext.Products.Include(x => x.Brand)
+                                            .Include(x => x.HeDieuHanh)
+                                            .Include(x => x.Supplier)
+                                            .Include(x => x.Categories)
+                                            .Where(x => x.product_ID == id)
+                                            .FirstOrDefault();
+
+            return View("Detail", product);
         }
 
         [HttpGet]
         public IActionResult Create()
         {
-            return PartialView();
+            ViewBag.listBrand = new SelectList(dbContext.Brands.ToList(), "brand_ID", "brand_Name");
+            ViewBag.listSupplier = new SelectList(dbContext.Suppliers.ToList(), "supplier_ID", "supplier_Name");
+            ViewBag.listCategories = new SelectList(dbContext.Categories.ToList(), "categories_ID", "categories_Name");
+            ViewBag.listHeDieuHanh = new SelectList(dbContext.HeDieuHanhs.ToList(), "hdh_ID", "hdh_Name");
+            return View();
         }
-
         [HttpPost]
-        public IActionResult Create(Product newProduct)
+        public IActionResult Create(Product newProduct, IFormFile image)
         {
             if (ModelState.IsValid)
             {
-                if (newProduct != null)
+                if (image == null || image.Length == 0)
                 {
-                    dbContext.Products.Add(newProduct);
-                    dbContext.SaveChanges();
-                    TempData["notifyMSG"] = "Đã thếm sản phẩm thành công";
-                    return RedirectToAction("Index");
+                    newProduct.product_Image = "";
                 }
+                else
+                {
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "~/images/", image.FileName);
+                    var stream = new FileStream(path, FileMode.Create);
+                    image.CopyToAsync(stream);
+                    newProduct.product_Image = image.FileName;
+                }
+
+                dbContext.Products.Add(newProduct);
+                dbContext.SaveChanges();
+                TempData["notifyMSG"] = "Đã thếm sản phẩm thành công";
+                return RedirectToAction("Index");
             }
+            TempData["notifyMSG"] = "Thêm sản phẩm thất bại!!!";
+            return View(newProduct);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            var oldProduct = dbContext.Products.Include(p => p.Brand)
+                                               .Include(p => p.Supplier)
+                                               .Include(p => p.HeDieuHanh)
+                                               .Include(p => p.Categories)
+                                               .Where(x => x.product_ID == id)
+                                               .FirstOrDefault();
+            return View(oldProduct);
+        }
+        [HttpPost]
+        public IActionResult ConfirmDelete(int? id)
+        {
+            if (id != null)
+            {
+                var chooseProduct = dbContext.Products.Include(p => p.Brand)
+                                                      .Include(p => p.Supplier)
+                                                      .Include(p => p.HeDieuHanh)
+                                                      .Include(p => p.Categories)
+                                                      .Where(x => x.product_ID == id)
+                                                      .FirstOrDefault();
+
+                dbContext.Remove(chooseProduct);
+                dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index", "ManageProduct");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            Product product = dbContext.Products.Include(p => p.Brand)
+                                                .Include(p => p.Supplier)
+                                                .Include(p => p.HeDieuHanh)
+                                                .Include(p => p.Categories)
+                                                .Where(x => x.product_ID == id)
+                                                .FirstOrDefault();
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult Edit(int? id, Product newProduct)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            Product oldProduct = dbContext.Products.Include(p => p.Brand)
+                                                   .Include(p => p.Supplier)
+                                                   .Include(p => p.HeDieuHanh)
+                                                   .Include(p => p.Categories)
+                                                   .Where(x => x.product_ID == id)
+                                                   .FirstOrDefault();
+
+            if (ModelState.IsValid)
+            {
+                oldProduct.product_Name = newProduct.product_Name;
+                oldProduct.product_Price = newProduct.product_Price;
+                oldProduct.product_Quantity = newProduct.product_Quantity;
+                oldProduct.product_Image = newProduct.product_Image;
+                oldProduct.product_Description = newProduct.product_Description;
+                oldProduct.brand_ID = newProduct.brand_ID;
+                oldProduct.hdh_ID = newProduct.hdh_ID;
+                oldProduct.supplier_ID = newProduct.supplier_ID;
+                oldProduct.categories_ID = newProduct.categories_ID;
+
+                return RedirectToAction("Index");
+            }
+
             return View(newProduct);
         }
     }

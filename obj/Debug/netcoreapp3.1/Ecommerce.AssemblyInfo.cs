7b543b76ee1084ff7282aac5c5938f4b7e21ff6f
Hau Nguyen <huutho1999@gmail.com>
2020-12-15 04:37:55 +0700

    Cau hinh upload hosting

--- obj/Debug/netcoreapp3.1/Ecommerce.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/Ecommerce.AssemblyInfo.cs
@@ -12,6 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap4")]
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("fd32f857-6138-4447-b637-b6aa074b5bc6")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
